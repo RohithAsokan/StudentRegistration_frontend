481e5a82dfa974a32971f49fe9e6f5e5a81de80d
RohithAsokan <90841555+RohithAsokan@users.noreply.github.com>
2024-11-27 00:20:58 +0530

    Update HomePage.js

--- HomePage.js
+++ HomePage.js
@@ -4,8 +4,8 @@ const HomePage = () => {
     return (
         <div style={{ textAlign: "center", marginTop: "60px" }}>
             <h1 style={{fontSize:"xxx-large", marginBottom:"60px"}}>Student Registration</h1>
-            <button onClick={() => (window.location.href = "/login")}>Login</button>
-            <button onClick={() => (window.location.href = "/register")}>Register</button>
+            <button onClick={() => (window.location.href = "/login")} style={{width:"10%", marginRight:"10px"}}>Login</button>
+            <button onClick={() => (window.location.href = "/register")} style={{width:"10%", marginLeft:"10px"}}>Register</button>
         </div>
     );
 };
