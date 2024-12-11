b0a2a63ab802100f0d313ca0638887027a8c4147
RohithAsokan <90841555+RohithAsokan@users.noreply.github.com>
2024-12-11 09:36:57 +0530

    Update AdminPage.js

--- AdminPage.js
+++ AdminPage.js
@@ -4,26 +4,39 @@ import "./style.css";
 function AdminPage() {
   const [showForm, setShowForm] = useState(false);
   const [courses, setCourses] = useState([]);
-  const [course, setCourse] = useState({ courseId: "", courseName: "", courseDescription: "" });
+  const [course, setCourse] = useState({ courseId: "", courseName: "", courseDescription: "", credits: 0});
   const [loading, setLoading] = useState(true);
   const username = "admin";
+
+  const fetchCourses = async () => {
+    setLoading(true);
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await fetch(`http://localhost:8080/getcourses`, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setCourses(data);
+      } else {
+        console.error("Error fetching courses:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchCourses = async () => {
-      try {
-        const response = await fetch(`http://localhost:8080/getcourses`);
-        if (response.ok) {
-          const data = await response.json();
-          setCourses(data);
-        }
-      }
-      finally {
-        setLoading(false);
-      }
-    };
     fetchCourses();
   }, []);
 
-  const toggleForm = () => setShowForm(!showForm);
+  const toggleForm = () => setShowForm((prev) => !prev);
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -32,34 +45,42 @@ function AdminPage() {
 
   const addCourse = async (e) => {
     e.preventDefault();
+    const token = localStorage.getItem("token");
+
+    if (!course.courseId || !course.courseName || !course.courseDescription || !course.credits) {
+      alert("All fields are required");
+      return;
+    }
+
     if (courses.some((c) => c.courseId === course.courseId)) {
       alert("A course with this Course ID already exists.");
       return;
     }
 
-    if (course.courseId && course.courseName && course.courseDescription) {
-      try {
-        const response = await fetch(`http://localhost:8080/addcourse`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(course),
-        });
+    try {
+      const response = await fetch(`http://localhost:8080/addcourse`, {
+        method: "POST",
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(course),
+      });
 
-        if (response.ok) {
-          const savedCourse = await response.json();
-          setCourses([...courses, savedCourse]);
-          setCourse({ courseId: "", courseName: "", courseDescription: "" });
-          setShowForm(false);
-        } 
-      } 
-      catch (error) {
-        console.error("Error while adding course:", error.message);
+      const message = await response.text();
+
+      if (response.ok) {
+        setCourses([...courses, course]);
+        setCourse({ courseId: "", courseName: "", courseDescription: "", credits: 0});
+        setShowForm(false);
+      } else {
+        alert(message);
       }
+    } catch (error) {
+      console.error(error.message);
     }
   };
-
+  
   return (
     <div className="admin-container">
       <h1 className="admin-welcome">Welcome, {username}</h1>
@@ -79,7 +100,11 @@ function AdminPage() {
           </div>
           <div className="form-group">
             <label className="form-label">Enter Course Description:</label>
-            <textarea name="courseDescription" value={course.courseDescription} onChange={handleInputChange} className="form-input" required></textarea>
+            <textarea name="courseDescription" value={course.courseDescription} onChange={handleInputChange} className="form-input" required ></textarea>
+          </div>
+          <div className="form-group">
+            <lable className="form-lable">Enter the Credits for Course</lable>
+            <input type="number" name="credits" value={course.credits} onChange={handleInputChange} className="form-input" required></input>
           </div>
           <button type="submit" className="form-submit-btn">Add</button>
         </form>
@@ -95,6 +120,7 @@ function AdminPage() {
                 <th>Course ID</th>
                 <th>Course Name</th>
                 <th>Course Description</th>
+                <th>Course Credits</th>
               </tr>
             </thead>
             <tbody>
@@ -103,13 +129,14 @@ function AdminPage() {
                   <td>{course.courseId}</td>
                   <td>{course.courseName}</td>
                   <td>{course.courseDescription}</td>
+                  <td>{course.credits}</td>
                 </tr>
               ))}
             </tbody>
           </table>
         </div>
       ) : (
-        <p>No courses available. Add a new course to get started!</p>
+        <p>No courses available.</p>
       )}
     </div>
   );
